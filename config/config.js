b8d925016873dacfda9bf1d951b5b83cd1791fb1
mesafnt <mesafnt.kalu@ucdenver.edu>
2023-05-01 11:55:28 -0600

    update

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 8080,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
       'mongodb+srv://node1:apigee@finalproject.j0dp51a.mongodb.net/api?retryWrites=true&w=majority',
       
-  stripe_connect_test_client_id: 'ca_JADPD4HoVcucYXzVg3ecepHKyiaC85o1',
-  stripe_test_secret_key: 'sk_test_51IXsL8EXPIkGMjJvlTGFpJzRox8Wau9lmdJmQgxzR0KNiY57XmNYOssHKr1Z1M5CsW07oRr5auw9MQTaVtFeOfAS00aJgm4k5y',
-  stripe_test_api_key: 'pk_test_51IXsL8EXPIkGMjJvpCqoLGYVpQQeZXBw50BYZMjUBRX1n7HgwrAolU9P4oBlcz29LThXENz1rcMsmoxhw0jhAFQs003lXLFuwf'
+  // stripe_connect_test_client_id: 'ca_JADPD4HoVcucYXzVg3ecepHKyiaC85o1',
+  // stripe_test_secret_key: 'sk_test_51IXsL8EXPIkGMjJvlTGFpJzRox8Wau9lmdJmQgxzR0KNiY57XmNYOssHKr1Z1M5CsW07oRr5auw9MQTaVtFeOfAS00aJgm4k5y',
+  // stripe_test_api_key: 'pk_test_51IXsL8EXPIkGMjJvpCqoLGYVpQQeZXBw50BYZMjUBRX1n7HgwrAolU9P4oBlcz29LThXENz1rcMsmoxhw0jhAFQs003lXLFuwf'
 }
 
 export default config
